ef6ca733012ca23a4376be276446d98b465703e2
Abisha Muthukrishnan <110807012+Abida-M@users.noreply.github.com>
2022-09-24 09:16:22 +0530

    Create inverted half triangle in numbers.py

---
+++ inverted half triangle in numbers.py
@@ -0,0 +1,10 @@
+r = int (input ("enter rows:")) 
+i = r 
+while ((i <= r)and (i!=0)):
+  j = 1 
+  while (j <= i):
+    print ((j), end="")
+    j += 1 
+  print ("\n") 
+  i = i - 1 
+print("The end")
